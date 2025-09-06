0abed4ab9112f242734f1f6f31174a970bfafc8e
ByatesEduard <edikbatec1@gmail.com>
2025-09-06 13:53:39 +0300

    add solution

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -1,16 +1,25 @@
 import { Good } from '../types/Good';
 
-// eslint-disable-next-line
-const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
+const API_URL = 'https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json';
 
+// завантажує всі товари
 export function getAll(): Promise<Good[]> {
   return fetch(API_URL).then(response => response.json());
 }
 
-export const get5First = () => {
-  return getAll().then(goods => goods); // sort and get the first 5
+// завантажує перші 5 (відсортовані за назвою)
+export const get5First = async (): Promise<Good[]> => {
+  const goods = await getAll();
+
+  return goods
+    .slice()
+    .sort((a, b) => a.name.localeCompare(b.name))
+    .slice(0, 5);
 };
 
-export const getRedGoods = () => {
-  return getAll().then(goods => goods); // get only red
+// завантажує лише червоні товари
+export const getRedGoods = async (): Promise<Good[]> => {
+  const goods = await getAll();
+
+  return goods.filter(good => good.color === 'red');
 };
